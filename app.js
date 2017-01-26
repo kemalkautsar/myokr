ffcf3db51d6fd4b1447b661d480d3d7857f608af
kemalkautsar <kemal.kautsar@gmail.com>
2017-01-26 16:14:50 +0700

    initial but not run-able
    idem

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path  = require('path');
+
+var okr = require('./routes/okr');
+var app = express();
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
+
+
+
+
+var connection = require('express-myconnection');
+var mysql = require('mysql');
+
+app.set('port', process.env.PORT || 1488);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(
+	connection(mysql,{
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		port: 3306,
+		database: 'okrdb'
+	}, 'pool')
+);
+
+app.get('/', routes.index);
+app.get('/okr/add', okr.add);
+app.post('/okr/add', okr.save);
+app.get('/okr/delete:id',okr.delete_okr);
+app.get('/okr/edit:id',okr.edit);
+app.post('/okr/edit',okr.save_edit);
+
+//app.use(app.router);
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('OKR application is running on localhost:' + app.get('port'));
+});
---
+++ routes/okr.js
@@ -0,0 +1,84 @@
+exports.list = function(req,res){
+	req.getConnection(function(err,connection){
+		console.log(connection);
+		var query = connection.query('SELECT * FROM keyResult', 
+			function(err,rows){
+				if(err)
+					console.log('error selecting db');
+				res.render('okr',{page_title:"okr list",data:rows});
+			});
+	});
+};
+
+exports.add = function(req,res){
+	res.render('add Objective',{page_title:"add objective"});
+};
+
+exports.edit = function(req,res){
+	var id = req.params.id;
+
+	req.getConnection(function(err,connection){
+		var query = connection.query('SELECT * FROM keyResult WHERE key_id = ?',[id], function(err,rows){
+			if (err)
+				console.log("error selecting : %s", err);
+			res.render('edit_okr',{page_title:"Edit OKR", data:rows});
+		});
+	});
+};
+
+exports.save = function(req,res){
+    
+    var input = JSON.parse(JSON.stringify(req.body));
+    
+    req.getConnection(function (err, connection) {
+        
+        var data = {
+            key_name	: input.key_name,
+			key_score	: input.key_score
+        
+        };
+        
+        var query = connection.query("INSERT INTO keyResult set ? ",data, function(err, rows)
+        {
+  
+          if (err)
+              console.log("Error inserting : %s ",err );
+         
+          res.redirect('/okr');
+          
+        });
+        
+       // console.log(query.sql); get raw query
+    
+    });
+};
+
+exports.save_edit = function(req,res){
+	var input = JSON.parse(JSON.stringify(req.body));
+	var id = req.params.id;
+
+	req.getConnection(function(err,connection){
+		var data = {
+			key_name	: input.key_name,
+			key_score	: input.key_score
+		};
+
+		connection.query("UPDATE keyResult set ? WHERE key_id = ? ",[data,id], function(err,rows){
+			if(err)
+				console.log("error updating : %s ",err);
+			res.redirect('/okr');
+		});
+	});
+};
+
+exports.delete_okr = function(req,res){
+	var id = req.params.id;
+
+	req.getConnection(function(err,connection){
+		connection.query("DELETE FROM keyResult WHERE id = ?", [id],function(err,rows){
+			if(err)
+				console.log("error deleting : s", err);
+			res.redirect('/okr');
+		});
+	});
+};
